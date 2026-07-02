548dfef2f4f1c117be8cb9f85054aa4d51f70491
Tanu Kumari <tannukumari56009@gmail.com>
2026-07-02 14:31:58 +0530

    Completed Logging Framework

--- Week-1/Design-Pattern-and-Principles/Exercise-2-FactoryMethodPattern/src/com/cognizant/factory/documents/WordDocument.java
+++ Week-1/Design-Pattern-and-Principles/Exercise-2-FactoryMethodPattern/src/com/cognizant/factory/documents/WordDocument.java
@@ -1,6 +1,6 @@
 package com.cognizant.factory.documents;
 
-public class WordDocument implements Document {
+public class WordDocument implements com.cognizant.factory.documents.Document {
 
     @Override
     public void open() {
