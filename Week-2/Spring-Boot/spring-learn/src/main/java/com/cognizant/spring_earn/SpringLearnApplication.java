a75f1d5a026d5df18c3f51b8b9f1744be501cb78
Tanu Kumari <tannukumari56009@gmail.com>
2026-07-09 18:07:51 +0530

    Completed Week 2 model

--- Week-2/Spring-Boot/spring-learn/src/main/java/com/cognizant/spring_earn/SpringLearnApplication.java
+++ Week-2/Spring-Boot/spring-learn/src/main/java/com/cognizant/spring_earn/SpringLearnApplication.java
@@ -1,13 +1,29 @@
 package com.cognizant.spring_earn;
 
+import com.cognizant.spring_earn.model.Country;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringLearnApplication.class, args);
+
+		displayCountry();
 	}
 
+	public static void displayCountry() {
+
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("country.xml");
+
+		Country country = context.getBean("country", Country.class);
+
+		System.out.println(country);
+
+	}
 }
